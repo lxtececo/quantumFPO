de6c02591456d63d6c8e7ac2bfadcfa24ee58784
Alexander Nachtigall <alexander.nachtigall@lxtececo.de>
2025-09-23 21:51:55 +0200

    Updates

--- backend/src/main/java/com/quantumfpo/stocks/controller/StockControllerTest.java
+++ backend/src/main/java/com/quantumfpo/stocks/controller/StockControllerTest.java
@@ -1,6 +1,5 @@
 package com.quantumfpo.stocks.controller;
 
-import com.quantumfpo.stocks.model.OptimizeRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
