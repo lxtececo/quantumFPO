0fcaf0d61cce0dccade0bebfd3e957e29233dbf7
Alexander Nachtigall <alexander.nachtigall@lxtececo.de>
2025-09-25 13:04:15 +0200

    Various updates

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>'],
+  testMatch: [
+    '<rootDir>/test/**/*.test.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx,ts,tsx}'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
+};
